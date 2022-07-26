198bbdb10c514e41d4bdc361d5c9cdafe4904168
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-07-26 13:16:52 +0000

    refactor: update user router
    Create mutations for :
    - user registration
    - user login
    - refresh user access token

--- packages/auth-server/src/trpc/routers/user.ts
+++ packages/auth-server/src/trpc/routers/user.ts
@@ -1,140 +1,119 @@
 import { createRouter } from "../router";
-import { z } from "zod";
-import { prisma } from "../../utils/prisma-client";
-import argon2 from "argon2";
-import { twilio } from "../../utils/twilio-client";
-import { encode } from "../../jwt";
+import { createUserInputSchema } from "../../schemas/CreateUserInput";
+import {
+  createUser,
+  findUniqueUser,
+  findUser,
+  generateTokens,
+} from "../../services/user.service";
 import { TRPCError } from "@trpc/server";
+import argon2 from "argon2";
+import { loginUserInputSchema } from "../../schemas/LoginUserInput";
+import { sendAccessToken, sendTokens } from "../../utils/send-token";
+import { decode, encode } from "../../jwt";
 
 export const userRouter = createRouter()
-  .query("getUser", {
-    input: z.string(),
-    async resolve(req) {
-      const user = await prisma.user
-        .findFirst({ where: { id: req.input } })
-        .finally(() => prisma.$disconnect);
-
-      return user;
-    },
-  })
-  .mutation("createUser", {
-    input: z.object({
-      name: z.string().min(5),
-      phone: z.string().min(10),
-      password: z.string().min(8),
-    }),
-    async resolve(req) {
-      const { phone, password, name } = req.input;
-
-      let user = await prisma.user
-        .findFirst({ where: { phone } })
-        .finally(() => prisma.$disconnect);
+  .mutation("register", {
+    input: createUserInputSchema,
+    async resolve({ ctx, input }) {
+      let user = await findUser({ where: { phone: input.phone } });
 
       if (user)
         throw new TRPCError({
-          code: "BAD_REQUEST",
-          message: "Le numéro de téléphone est déjà enregistré.",
+          code: "CONFLICT",
+          message: "Numéro de téléphone déjà utilisé.",
         });
 
-      const hashedPassword = await argon2.hash(password);
+      const hashedPassword = await argon2.hash(input.password);
 
-      const newUser = await prisma.user
-        .create({
-          data: { phone, name, password: hashedPassword },
-          select: {
-            id: true,
-            phone: true,
-            indicatif: true,
-          },
-        })
-        .finally(() => prisma.$disconnect);
+      user = await createUser({
+        name: input.name,
+        password: hashedPassword,
+        phone: input.phone,
+      });
 
       return {
         id: null,
         result: {
           type: "data",
-          data: newUser,
+          data: user,
         },
       };
     },
   })
-  .mutation("sendVerification", {
-    input: z.object({
-      phone: z.string().length(10),
-      indicatif: z.string(),
-      id: z.string(),
-    }),
-    async resolve({ input }) {
-      const { phone, indicatif } = input;
+  .mutation("login", {
+    input: loginUserInputSchema,
+    async resolve({ ctx, input }) {
+      let user = await findUser({ where: { phone: input.phone } });
 
-      return twilio.verifications
-        .create({
-          to: `${indicatif}${phone}`,
-          channel: "sms",
-        })
-        .then((verification) => {
-          return { status: verification.status };
-        })
-        .catch((err) => {
-          console.error(err);
-          return { status: "failed to send request" };
+      if (!user || (await argon2.verify(user.password, input.password)))
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Identifiants invalides.",
         });
+
+      const { access_token, refresh_token } = await generateTokens(user.id);
+
+      // Send access and refresh tokens in the cookie
+      sendTokens({
+        ctx,
+        accessToken: access_token,
+        refreshToken: refresh_token,
+      });
+
+      return {
+        id: null,
+        result: {
+          type: "data",
+          data: access_token,
+          status: "success",
+        },
+      };
     },
   })
-  .mutation("verify", {
-    input: z.object({
-      phone: z.string().length(10),
-      indicatif: z.string(),
-      code: z.number().int(),
-    }),
-    async resolve({ ctx, input }) {
-      const { phone, indicatif, code } = input;
+  .mutation("refreshAccessToken", {
+    async resolve({ ctx }) {
+      const refreshToken = ctx.req.cookies?.voodoo_refresh_token as string;
 
-      return twilio.verificationChecks
-        .create({ to: `${indicatif}${phone}`, code: code.toString() })
-        .then(async (verification_check) => {
-          if (verification_check.status === "approved") {
-            const user = await prisma.user.findFirst({
-              where: { phone },
-              select: { id: true },
-            });
-
-            if (!user)
-              return { status: "failed", message: "The user does not exist." };
-
-            ctx.res.cookie(
-              "refresh_token",
-              await encode({
-                userId: user.id,
-                expiresIn: "7d",
-                type: "refresh",
-              }),
-              { httpOnly: true }
-            );
-
-            ctx.res.cookie(
-              "access_token",
-              await encode({
-                userId: user.id,
-                expiresIn: "15m",
-                type: "access",
-              }),
-              { httpOnly: true }
-            );
-
-            return {
-              accessToken: await encode({
-                userId: user.id,
-                expiresIn: "15m",
-                type: "access",
-              }),
-            };
-          }
-          return { status: verification_check.status };
-        })
-        .catch((err) => {
-          console.error(err);
-          return { status: "failed to send request" };
+      if (!refreshToken)
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: "Impossible de rafraîchir le jeton d'accès.",
         });
+
+      const payload = await decode({
+        token: refreshToken,
+        secret: process.env.REFRESH_TOKEN_SECRET!,
+      });
+
+      if (!payload)
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: "Impossible de rafraîchir le jeton d'accès.",
+        });
+
+      const user = await findUniqueUser({ where: { id: payload.userId } });
+
+      if (!user)
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: "Impossible de rafraîchir le jeton d'accès.",
+        });
+
+      const accessToken = await encode({
+        userId: user.id,
+        expiresIn: "15m",
+        type: "access",
+      });
+
+      sendAccessToken({ ctx, accessToken });
+
+      return {
+        id: null,
+        result: {
+          type: "data",
+          data: accessToken,
+        },
+      };
     },
   });
