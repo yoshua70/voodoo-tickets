4ed4001aec5fb0b7492ce7e8f58fede767aa7046
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-07-27 11:53:28 +0000

    feat: add user phone verification
    Add mutation for sending OTP to phone user.
    Add mutation for verify OTP from user.

---
+++ packages/auth-server/src/trpc/routers/auth.ts
@@ -0,0 +1,94 @@
+import { createRouter } from "../router";
+import { z } from "zod";
+import { twilio } from "../../utils/twilio-client";
+import { TRPCError } from "@trpc/server";
+import { findUser, generateTokens } from "../../services/user.service";
+
+const sendVerificationCodeSchema = z.object({
+  phone: z.string().min(10),
+  diallingCode: z.string().min(1),
+});
+
+const verifyCodeSchema = z.object({
+  phone: z.string().min(10),
+  diallingCode: z.string().min(1),
+  code: z.string().length(6),
+});
+
+export const authRouter = createRouter()
+  .mutation("sendVerificationCode", {
+    input: sendVerificationCodeSchema,
+    async resolve({ ctx, input }) {
+      const { phone, diallingCode } = input;
+
+      return twilio.verifications
+        .create({
+          to: `${diallingCode}${phone}`,
+          channel: "sms",
+        })
+        .then((verification) => {
+          return {
+            id: null,
+            result: {
+              type: "data",
+              data: verification.status,
+            },
+          };
+        })
+        .catch((err) => {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Failed to send request.",
+            cause: err,
+          });
+        });
+    },
+  })
+  .mutation("verify", {
+    input: verifyCodeSchema,
+    async resolve({ ctx, input }) {
+      const { phone, diallingCode, code } = input;
+
+      return twilio.verificationChecks
+        .create({ to: `${diallingCode}${phone}`, code: code.toString() })
+        .then(async (verification_check) => {
+          if (verification_check.status !== "approved") {
+            throw new TRPCError({
+              code: "BAD_REQUEST",
+              message: "Code invalide",
+            });
+          }
+
+          const user = await findUser({
+            where: { phone, diallingCode },
+            select: { id: true },
+          });
+
+          if (!user)
+            throw new TRPCError({
+              code: "BAD_REQUEST",
+              message: "Aucun utilisateur associé à ce numéro de téléphone.",
+            });
+
+          const { access_token, refresh_token } = await generateTokens(user.id);
+
+          return {
+            id: null,
+            result: {
+              type: "data",
+              data: {
+                access_token,
+                refresh_token,
+              },
+            },
+          };
+        })
+        .catch((err) => {
+          console.error(err);
+          throw new TRPCError({
+            code: "CONFLICT",
+            cause: err,
+          });
+        });
+    },
+  });
--- packages/auth-server/src/trpc/server.ts
+++ packages/auth-server/src/trpc/server.ts
@@ -1,6 +1,9 @@
 import { createRouter } from "./router";
+import { authRouter } from "./routers/auth";
 import { userRouter } from "./routers/user";
 
-export const appRouter = createRouter().merge("user.", userRouter);
+export const appRouter = createRouter()
+  .merge("user.", userRouter)
+  .merge("auth.", authRouter);
 
 export type AppRouter = typeof appRouter;
