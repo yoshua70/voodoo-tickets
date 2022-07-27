2e25487792e47001fff7cc477ae20ac0aa501971
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-07-27 10:04:21 +0000

    feat: set protected route
    Make the route accessible for signed in users.
    Redirect user to sign-up when no access_token is present.

--- packages/web-app/pages/dashboard.tsx
+++ packages/web-app/pages/dashboard.tsx
@@ -3,20 +3,33 @@ import {
   InferGetServerSidePropsType,
   NextPage,
 } from "next";
+import { useCookies } from "react-cookie";
 import { Layout } from "../components/Layout";
 import { parseCookie } from "../utils/parse-cookie";
-import { trpc } from "../utils/trpc";
 
 const Dashboard: NextPage = ({
   cookie,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log(cookie);
+  const [tokensCookies, setTokensCookies, removeTokenscookies] = useCookies([
+    "voodoo_access_token",
+    "voodoo_refresh_token",
+  ]);
 
+  const signOut = () => {
+    removeTokenscookies("voodoo_access_token");
+    removeTokenscookies("voodoo_refresh_token");
+  };
   return (
     <Layout title="Tableau de bord">
-      <h1>Tableau de bord</h1>
-      <p>{cookie?.voodoo_access_token}</p>
-      <p>{cookie?.voodoo_refresh_token}</p>
+      <div className="flex justify-between p-4">
+        <h1>Tableau de bord</h1>
+        <button
+          onClick={() => signOut()}
+          className="px-4 py-2 bg-red-500 rounded"
+        >
+          Sign Out
+        </button>
+      </div>
     </Layout>
   );
 };
@@ -26,9 +39,10 @@ export default Dashboard;
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const cookie = parseCookie(req);
 
-  if (!cookie) return { redirect: { destination: "/sign-up" }, props: {} };
+  if (!cookie.voodoo_access_token)
+    return { redirect: { destination: "/sign-up" }, props: {} };
 
   return {
-    props: { cookie },
+    props: { access_token: cookie.voodoo_access_token },
   };
 };
